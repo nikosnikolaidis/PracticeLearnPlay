2576060b063a6e79b852c0a9f057cb0e29202bf3
Owner <Owner@mostrous-4dbba5>
2015-05-19 20:05:48 +0300

    pros8eseis sthn exercise k stis apo katw ths

--- PLP/src/ergasia/Grammar.java
+++ PLP/src/ergasia/Grammar.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Grammar extends Exercise {
 	
-	private ArrayList<Question> listOfAllGrammarQuestions;
+	private ArrayList<Question> listOfAllGrammarQuestionsWithTheSameLevel;
 	
 	public Grammar(Language language, String level) {
 		super(language,level);
@@ -14,7 +14,15 @@ public class Grammar extends Exercise {
 
 	//dhmiourgia askhseologiou
 	//dhmiourgeitai ena plh8os diadoxikwn erwthsewn typou Grammar, stis opoies 8a e3etastei o student
-	public void createExercise(){
-	
+	public ArrayList<Question> createTest(){
+		ArrayList<Question> testList = new ArrayList<Question>();
+		
+		return testList;
+	}
+
+	@Override
+	public int answerEvaluation() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
--- PLP/src/ergasia/Listening.java
+++ PLP/src/ergasia/Listening.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Listening extends Exercise {
 	
-	private ArrayList<AudioFile> audioFileList;
+	//private ArrayList<AudioFile> audioFileListWithTheSameLevel;
 	
 	public Listening(Language language, String level) {
 		super(language,level);
@@ -13,8 +13,17 @@ public class Listening extends Exercise {
 
 	//dhmiourgia askhseologiou
 	//dhmiourgeitai ena plh8os diadoxikwn erwthsewn typou Listening, stis opoies 8a e3etastei o student
-	public void createExercise() {
-	
+	public ArrayList<Question> createTest() {
+		ArrayList<Question> testList = new ArrayList<Question>();
+		
+		return testList;
 	
 }
+
+
+	@Override
+	public int answerEvaluation() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
