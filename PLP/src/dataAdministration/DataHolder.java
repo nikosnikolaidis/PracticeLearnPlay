8a58a100c70d3dff7a3bb83a8270f32fe82cf5b0
Nikos <Nikos@Nikos>
2015-05-27 21:25:19 +0300

    DataHolder and Serializator

--- PLP/src/dataAdministration/DataHolder.java
+++ PLP/src/dataAdministration/DataHolder.java
@@ -1,13 +1,80 @@
 package dataAdministration;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-
+import java.util.HashMap;
 import ergasia.*;
 
 public class DataHolder {
 
-	private ArrayList<Language> Languages;
-	private ArrayList<Teacher> Teachers;
-	private ArrayList<Student> Students;
+	private Language languageNow;	//i glosa pou uparxei gia auti tin isodo
+	
+	private ArrayList<Language> Languages;	//lista me oles tis gloses
+	private ArrayList<Level> Levels;		//lista me ola ta levels
+	private ArrayList<Teacher> Teachers;	//lista me olous tous ka8igites
+	private ArrayList<Student> Students;	//lista me olous tous ma8ites
+	
+	private HashMap<Question,Level> listOfAllGrammarQuestions;	//ola ta Grammar Questions
+	private HashMap<Question,Level> listOfAllVocabularyQuestions;	//ola ta Vocabulary Questions
+	private HashMap<Question,Level> listOfAllListeningQuestions;	//ola ta Listening Questions
+	private HashMap<Question,Level> listOfAllReadingQuestions;		//ola ta Reading Questions
+	
+	public Language getLanguageNow() {
+		return languageNow;
+	}
+	public void setLanguageNow(Language languageNow) {
+		this.languageNow = languageNow;
+	}
+	public ArrayList<Language> getLanguages() {
+		return Languages;
+	}
+	public void setLanguages(ArrayList<Language> languages) {
+		Languages = languages;
+	}
+	public ArrayList<Level> getLevels() {
+		return Levels;
+	}
+	public void setLevels(ArrayList<Level> levels) {
+		Levels = levels;
+	}
+	public ArrayList<Teacher> getTeachers() {
+		return Teachers;
+	}
+	public void setTeachers(ArrayList<Teacher> teachers) {
+		Teachers = teachers;
+	}
+	public ArrayList<Student> getStudents() {
+		return Students;
+	}
+	public void setStudents(ArrayList<Student> students) {
+		Students = students;
+	}
+	public HashMap<Question, Level> getListOfAllGrammarQuestions() {
+		return listOfAllGrammarQuestions;
+	}
+	public void setListOfAllGrammarQuestions(
+			HashMap<Question, Level> listOfAllGrammarQuestions) {
+		this.listOfAllGrammarQuestions = listOfAllGrammarQuestions;
+	}
+	public HashMap<Question, Level> getListOfAllVocabularyQuestions() {
+		return listOfAllVocabularyQuestions;
+	}
+	public void setListOfAllVocabularyQuestions(
+			HashMap<Question, Level> listOfAllVocabularyQuestions) {
+		this.listOfAllVocabularyQuestions = listOfAllVocabularyQuestions;
+	}
+	public HashMap<Question, Level> getListOfAllListeningQuestions() {
+		return listOfAllListeningQuestions;
+	}
+	public void setListOfAllListeningQuestions(
+			HashMap<Question, Level> listOfAllListeningQuestions) {
+		this.listOfAllListeningQuestions = listOfAllListeningQuestions;
+	}
+	public HashMap<Question, Level> getListOfAllReadingQuestions() {
+		return listOfAllReadingQuestions;
+	}
+	public void setListOfAllReadingQuestions(
+			HashMap<Question, Level> listOfAllReadingQuestions) {
+		this.listOfAllReadingQuestions = listOfAllReadingQuestions;
+	}
+	
 }
--- PLP/src/dataAdministration/Serializator.java
+++ PLP/src/dataAdministration/Serializator.java
@@ -1,5 +1,272 @@
 package dataAdministration;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import ergasia.*;
+
 public class Serializator {
 
+	private DataHolder DH =new DataHolder();
+	
+	public ArrayList<Teacher> TeachersDeserializing(){						//Teachers Deserializing
+		ArrayList<Teacher> emp=DH.getTeachers();
+		try {
+			FileInputStream fileIn = new FileInputStream("teachers.ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			emp = (ArrayList<Teacher>) in.readObject();
+			in.close();
+			fileIn.close();
+		}
+		catch(IOException i) {
+			i.printStackTrace();
+		}
+		catch(ClassNotFoundException c) {
+			c.printStackTrace();
+		}
+		finally {
+			return emp;
+		}
+	}
+	public void TeachersSerializing(ArrayList<Teacher> Teachers){			//Teachers Serializing
+		try {
+			FileOutputStream fileOut = new FileOutputStream("teachers.ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(Teachers);
+			out.close();
+			fileOut.close();
+		}
+		catch(IOException i) {
+			i.printStackTrace();
+		}
+	}
+	
+	public ArrayList<Student> StudentDeserializing() {						//Student Deserializing
+		ArrayList<Student> emp=DH.getStudents();
+		try {
+			FileInputStream fileIn = new FileInputStream("students.ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			emp = (ArrayList<Student>) in.readObject();
+			in.close();
+			fileIn.close();
+		}
+		catch(IOException i) {
+			i.printStackTrace();
+		}
+		catch(ClassNotFoundException c) {
+			c.printStackTrace();
+		}
+		finally {
+			return emp;
+		}
+	}
+	public void StudentSerializing(ArrayList<Teacher> Students) {				//Student Serializing
+		try {
+			FileOutputStream fileOut = new FileOutputStream("students.ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(Students);
+			out.close();
+			fileOut.close();
+		}
+		catch(IOException i) {
+			i.printStackTrace();
+		}
+	}
+	
+	public ArrayList<Language> LanguageDeserializing() {						//Language Deserializing
+		ArrayList<Language> emp=DH.getLanguages();
+		try {
+			FileInputStream fileIn = new FileInputStream("languages.ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			emp = (ArrayList<Language>) in.readObject();
+			in.close();
+			fileIn.close();
+		}
+		catch(IOException i) {
+			i.printStackTrace();
+		}
+		catch(ClassNotFoundException c) {
+			c.printStackTrace();
+		}
+		finally {
+			return emp;
+		}
+	}
+	public void LanguageSerializing(ArrayList<Language> Languages) {			//Language Serializing
+		try {
+			FileOutputStream fileOut = new FileOutputStream("languages.ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(Languages);
+			out.close();
+			fileOut.close();
+		}
+		catch(IOException i) {
+			i.printStackTrace();
+		}
+	}
+	
+	public ArrayList<Level> LevelDeserializing() {						//Level Deserializing
+		ArrayList<Level> emp=DH.getLevels();
+		try {
+			FileInputStream fileIn = new FileInputStream("level.ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			emp = (ArrayList<Level>) in.readObject();
+			in.close();
+			fileIn.close();
+		}
+		catch(IOException i) {
+			i.printStackTrace();
+		}
+		catch(ClassNotFoundException c) {
+			c.printStackTrace();
+		}
+		finally {
+			return emp;
+		}
+	}
+	public void LevelSerializing(ArrayList<Level> Levels) {			//Level Serializing
+		try {
+			FileOutputStream fileOut = new FileOutputStream("level.ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(Levels);
+			out.close();
+			fileOut.close();
+		}
+		catch(IOException i) {
+			i.printStackTrace();
+		}
+	}
+	
+	public HashMap<Question,Level> GrammarQuestionsDeserializing() {			//GrammarQuestions Deserializing
+		HashMap<Question,Level> emp=DH.getListOfAllGrammarQuestions();
+		try {
+			FileInputStream fileIn = new FileInputStream("grammarQuestions.ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			emp = (HashMap<Question,Level>) in.readObject();
+			in.close();
+			fileIn.close();
+		}
+		catch(IOException i) {
+			i.printStackTrace();
+		}
+		catch(ClassNotFoundException c) {
+			c.printStackTrace();
+		}
+		finally {
+			return emp;
+		}
+	}
+	public void GrammarQuestionsSerializing(HashMap<Question,Level> grammarQuestions) {	//grammarQuestions Serializing
+		try {
+			FileOutputStream fileOut = new FileOutputStream("grammarQuestions.ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(grammarQuestions);
+			out.close();
+			fileOut.close();
+		}
+		catch(IOException i) {
+			i.printStackTrace();
+		}
+	}
+	
+	public HashMap<Question,Level> VocabularyQuestionsDeserializing() {			//VocabularyQuestions Deserializing
+		HashMap<Question,Level> emp=DH.getListOfAllVocabularyQuestions();
+		try {
+			FileInputStream fileIn = new FileInputStream("vocabularyQuestions.ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			emp = (HashMap<Question,Level>) in.readObject();
+			in.close();
+			fileIn.close();
+		}
+		catch(IOException i) {
+			i.printStackTrace();
+		}
+		catch(ClassNotFoundException c) {
+			c.printStackTrace();
+		}
+		finally {
+			return emp;
+		}
+	}
+	public void VocabularyQuestionsSerializing(HashMap<Question,Level> VocabularyQuestions) {	//VocabularyQuestions Serializing
+		try {
+			FileOutputStream fileOut = new FileOutputStream("vocabularyQuestions.ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(VocabularyQuestions);
+			out.close();
+			fileOut.close();
+		}
+		catch(IOException i) {
+			i.printStackTrace();
+		}
+	}
+	
+	public HashMap<Question,Level> ListeningQuestionsDeserializing() {			//ListeningQuestions Deserializing
+		HashMap<Question,Level> emp=DH.getListOfAllListeningQuestions();
+		try {
+			FileInputStream fileIn = new FileInputStream("listeningQuestions.ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			emp = (HashMap<Question,Level>) in.readObject();
+			in.close();
+			fileIn.close();
+		}
+		catch(IOException i) {
+			i.printStackTrace();
+		}
+		catch(ClassNotFoundException c) {
+			c.printStackTrace();
+		}
+		finally {
+			return emp;
+		}
+	}
+	public void ListeningQuestionsSerializing(HashMap<Question,Level> ListeningQuestions) {	//ListeningQuestions Serializing
+		try {
+			FileOutputStream fileOut = new FileOutputStream("listeningQuestions.ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(ListeningQuestions);
+			out.close();
+			fileOut.close();
+		}
+		catch(IOException i) {
+			i.printStackTrace();
+		}
+	}
+	
+	public HashMap<Question,Level> ReadingQuestionsDeserializing() {			//ReadingQuestions Deserializing
+		HashMap<Question,Level> emp=DH.getListOfAllReadingQuestions();
+		try {
+			FileInputStream fileIn = new FileInputStream("readingQuestions.ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			emp = (HashMap<Question,Level>) in.readObject();
+			in.close();
+			fileIn.close();
+		}
+		catch(IOException i) {
+			i.printStackTrace();
+		}
+		catch(ClassNotFoundException c) {
+			c.printStackTrace();
+		}
+		finally {
+			return emp;
+		}
+	}
+	public void ReadingQuestionsSerializing(HashMap<Question,Level> ReadingQuestions) {	//ReadingQuestions Serializing
+		try {
+			FileOutputStream fileOut = new FileOutputStream("readingQuestions.ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(ReadingQuestions);
+			out.close();
+			fileOut.close();
+		}
+		catch(IOException i) {
+			i.printStackTrace();
+		}
+	}
 }
