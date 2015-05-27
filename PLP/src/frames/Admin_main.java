7ddf02395e4407a1bdcf0373a895e6a3b3829503
Stergios <Stergios@Stergio-PC>
2015-05-27 22:46:26 +0300

    admin main frame

--- PLP/src/frames/Admin_main.java
+++ PLP/src/frames/Admin_main.java
@@ -1,9 +1,20 @@
 package frames;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-import javax.swing.*;
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Admin_main extends JFrame{
 	
@@ -11,16 +22,106 @@ public class Admin_main extends JFrame{
 	private JPanel panel;
 	
 	public Admin_main(){
-		buttonExit=new JButton("Exit");
-		buttonCreateTeacher=new JButton("Δημιουργία Καθηγητή");
+		
+		
 		buttonCreateLesson=new JButton("Δημιουργία Μαθήματος");
-		panel=new JPanel();
 		
-		panel.add(buttonCreateTeacher);
-		panel.add(buttonCreateLesson);
-		panel.add(buttonExit);
+		 BufferedImage background = null;
+			try {
+				background = ImageIO.read(new File("back.jpg"));
+			} 
+			catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			Image dbackground = background.getScaledInstance(1980, 1020, Image.SCALE_SMOOTH);
+ 		  JLabel backgroundLabel = new JLabel();
+ 		  backgroundLabel.setIcon(new javax.swing.ImageIcon(dbackground));
+ 		 this.setLayout(new BorderLayout());
+ 		 this.setContentPane(backgroundLabel);
+	     this.setLayout(new FlowLayout(FlowLayout.CENTER, 5000, 150));
 		
-		this.setContentPane(panel);
+	    
+		JPanel createTeacherPanel=new JPanel();
+		createTeacherPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+		
+		JLabel createTeacherLabel=new JLabel();
+		BufferedImage dcreateTeacherLabel = null;
+		try {
+			dcreateTeacherLabel = ImageIO.read(new File("createteacherlabel.png"));
+		} 
+		catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		Image createTeacherLabelImage = dcreateTeacherLabel.getScaledInstance(400, 150, Image.SCALE_SMOOTH);
+		createTeacherLabel.setIcon(new javax.swing.ImageIcon(createTeacherLabelImage));
+		
+		buttonCreateTeacher = new JButton("");
+		BufferedImage dcreateTeacherButton = null;
+		try {
+			dcreateTeacherButton = ImageIO.read(new File("createteacher.jpg"));
+		} 
+		catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		Image createTeacherButtonImage = dcreateTeacherButton.getScaledInstance(250, 200, Image.SCALE_SMOOTH);
+		buttonCreateTeacher.setIcon(new javax.swing.ImageIcon(createTeacherButtonImage));
+		buttonCreateTeacher.setPreferredSize(new Dimension(200, 150));
+	    
+		createTeacherPanel.add(createTeacherLabel);
+		createTeacherPanel.add(buttonCreateTeacher);
+		
+		this.add(createTeacherPanel);
+		
+		
+		JPanel createLessonPanel=new JPanel();
+		createLessonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+		
+		JLabel createLessonLabel=new JLabel();
+		BufferedImage dcreateLessonLabel = null;
+		try {
+			dcreateLessonLabel = ImageIO.read(new File("createlessonlabel.png"));
+		} 
+		catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		Image createLessonLabelImage = dcreateLessonLabel.getScaledInstance(400, 150, Image.SCALE_SMOOTH);
+		createLessonLabel.setIcon(new javax.swing.ImageIcon(createLessonLabelImage));
+		
+		buttonCreateLesson = new JButton();
+		BufferedImage dcreateLessonButton = null;
+		try {
+			dcreateLessonButton = ImageIO.read(new File("createlesson.jpg"));
+		} 
+		catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		Image createLessonButtonImage = dcreateLessonButton.getScaledInstance(270, 200, Image.SCALE_SMOOTH);
+		buttonCreateLesson.setIcon(new javax.swing.ImageIcon(createLessonButtonImage));
+		buttonCreateLesson.setPreferredSize(new Dimension(250, 150));
+	    
+		createLessonPanel.add(createLessonLabel);
+		createLessonPanel.add(buttonCreateLesson);
+		
+		this.add(createLessonPanel);
+		
+		buttonExit=new JButton();
+		BufferedImage dbuttonExit = null;
+		try {
+			dbuttonExit = ImageIO.read(new File("exit.jpg"));
+		} 
+		catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		Image buttonExitImage = dbuttonExit.getScaledInstance(270, 200, Image.SCALE_SMOOTH);
+		buttonExit.setIcon(new javax.swing.ImageIcon(buttonExitImage));
+		buttonExit.setPreferredSize(new Dimension(150, 150));
+		
+		
+		
+		
+		this.add(buttonExit);
+		
+		
 		
 		ButtonListener listener=new ButtonListener();
 		buttonExit.addActionListener(listener);
@@ -28,8 +129,8 @@ public class Admin_main extends JFrame{
 		buttonCreateLesson.addActionListener(listener);
 		
 		this.setVisible(true);
-		this.setSize(400,400);
-		this.setTitle("Event Example");
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		this.setTitle("PLP");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
@@ -40,13 +141,10 @@ class ButtonListener implements ActionListener {
 				setVisible(false);
 				dispose();
 			}else if(e.getSource()==buttonCreateTeacher){
-				setVisible(false);
-				dispose();
-				new Admin_createTeacher();
+				 
 			}else if(e.getSource()==buttonCreateLesson){
-				setVisible(false);
-				dispose();
-				new Admin_createLesson();
+				
+
 			}
 		}
 	}
